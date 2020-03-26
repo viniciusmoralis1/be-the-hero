2327f411af7ab3dec7d81529ea4b5e783fc1cde0
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-03-25 23:39:26 -0300

    finalizando backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const ongC = require('./controllers/OngControllers');
+const incC = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    evento: "Semana Omnistack 11",
-    acao: "iniciando o back-end"
-  });
-});
+routes.get('/ongs', ongC.index);
+routes.post('/ongs', ongC.create);
+
+routes.get('/incidents', incC.index);
+routes.post('/incidents', incC.create);
+routes.delete('/incidents/:id', incC.delete);
 
 module.exports = routes;
