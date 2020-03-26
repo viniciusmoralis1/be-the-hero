e16469994d33346e760983fedd49dfb231637de7
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-03-26 13:58:26 -0300

    configurando nova rota e cors

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const ongC = require('./controllers/OngControllers');
 const incC = require('./controllers/IncidentController');
+const proC = require('./controllers/ProfileController');
+const sesC = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongC.index);
 routes.post('/ongs', ongC.create);
+
+routes.post('/session', sesC.create);
+
+routes.get('/profile', proC.index);
 
 routes.get('/incidents', incC.index);
 routes.post('/incidents', incC.create);
