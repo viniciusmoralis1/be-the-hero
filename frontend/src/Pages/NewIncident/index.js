334dae1b291772fa3e2a57681be060c5e213eb94
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-03-27 20:46:08 -0300

    configurando acesso ao backend

--- frontend/src/Pages/NewIncident/index.js
+++ frontend/src/Pages/NewIncident/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident(){
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ongId = localStorage.getItem('ongId');
+
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title, description, value
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      })
+
+      history.push('/profile');
+
+    } catch(err){
+      alert('Erro ao cadastrar caso. Tente novamente');
+    }
+  }
+
   return (
     <div className="incident-container">
       <div className="content">
@@ -20,10 +51,10 @@ export default function NewIncident(){
               Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso"/>
-          <textarea placeholder="Descrição"/>
-          <input placeholder="Valor em reais"/>
+        <form onSubmit={handleNewIncident}>
+          <input placeholder="Título do caso" value={title} onChange={e => setTitle(e.target.value)} />
+          <textarea placeholder="Descrição" value={description} onChange={e => setDescription(e.target.value)} />
+          <input placeholder="Valor em reais" value={value} onChange={e => setValue(e.target.value)} />
 
           <button className="button" type="submit"> Cadastrar </button>
         </form>
